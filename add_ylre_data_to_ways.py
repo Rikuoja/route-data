0b9790109b28c217ba8dabec1219d08848e0fd17
Rikuoja <riku.oja@kapsi.fi>
2016-09-19 18:45:00 +0300

    Add general preferred and ignored polygons filters

--- add_ylre_data_to_ways.py
+++ add_ylre_data_to_ways.py
@@ -9,14 +9,12 @@ from collections import OrderedDict
 
 # first input must be a collection of linestrings
 talvi = fiona.open("talvi2016-2017.lines.geojson")
+# second input must be a collection of polygons
+ylre = fiona.open("ylre.shp")
 # second input must be a collection of polygons (should? be cleaned with e.g. Grass in the ylre case)
 # to be used preferentially
-ylre_pyora = fiona.open("ylre_pyoravaylat.shp")
+#ylre_pyora = fiona.open("ylre_pyoravaylat.shp")
 # third input must be a collection of polygons used as fallback if no match is found in the above network
-ylre = fiona.open("ylre.shp")
-# rtree index is required to calculate intersections in reasonable time
-index = rtree.index.Index()
-complete_index = rtree.index.Index()
 
 # fields to import from linestrings
 import_linestring_fields = {'id': 'original_line_id'}
@@ -36,13 +34,18 @@ import_polygon_fields = {'osan_id': 'ylre_id',
                     'aluetieto': 'area_type',
                     'alueen_nim': 'area_name',
                     'paivitetty': 'last_modified_time'}
+# polygons to import from preferentially
+preferred_polygon_filter = {'subtype_id': [6, 8, 9, 11]}  # bike lane, combined bike&pedestrian lane
+# polygons to ignore completely
+ignored_polygon_filter = {'subtype_id': [7],  # pedestrian zone
+                         'type_id': [23, 24]}  # lawns and plantations
 
 # linestring fields will override polygon fields
 import_fields_as = import_polygon_fields.copy()
 import_fields_as.update(import_linestring_fields)
 
 # create output schema automatically from input schema
-combined_properties = ylre_pyora.schema['properties'].copy()
+combined_properties = ylre.schema['properties'].copy()
 combined_properties.update(talvi.schema['properties'])
 import_fields_schema = {'geometry': talvi.schema['geometry'],
                         'properties': OrderedDict(
@@ -75,25 +78,24 @@ buffer_save = fiona.open("buffers.json",
                       schema={'properties': OrderedDict([('id', 'str')]), 'geometry': 'Polygon'})
 
 
-ylre_pyora_list = list(ylre_pyora)
-polygons = [shape(polygon['geometry']) for polygon in ylre_pyora_list]
+#ylre_pyora_list = list(ylre_pyora)
+#polygons = [shape(polygon['geometry']) for polygon in ylre_pyora_list]
+#polygon_metadata = [{import_fields_as[key]: value for key, value in polygon['properties'].items()
+#                     if key in import_fields_as}
+#                    for polygon in ylre_pyora_list]
+ylre_list = list(ylre)
+polygons = [shape(polygon['geometry']) for polygon in ylre_list]
 polygon_metadata = [{import_fields_as[key]: value for key, value in polygon['properties'].items()
                      if key in import_fields_as}
-                    for polygon in ylre_pyora_list]
-ylre_complete_list = list(ylre)
-all_polygons = [shape(polygon['geometry']) for polygon in ylre_complete_list]
-all_polygon_metadata = [{import_fields_as[key]: value for key, value in polygon['properties'].items()
-                     if key in import_fields_as}
-                    for polygon in ylre_complete_list]
+                    for polygon in ylre_list]
 
 print("Found area data")
 
+# rtree index is required to calculate intersections in reasonable time
+index = rtree.index.Index()
 for polygon_index, polygon in enumerate(polygons):
     index.insert(polygon_index, polygon.bounds)
-print("Generated cycling area index")
-for polygon_index, polygon in enumerate(all_polygons):
-    complete_index.insert(polygon_index, polygon.bounds)
-print("Generated complete area index")
+print("Generated area index")
 
 routes = [shape(linestring['geometry']) for linestring in talvi]
 route_metadata = [{import_fields_as[key]: value for key, value in route['properties'].items()
@@ -136,16 +138,23 @@ def add_to_new_linestrings(geometry, metadata):
 print("Found linestring data")
 
 
-# 1) split those parts of linestring that are within an area
+# 1) cut according to all boundaries crossed, direct match to any preferred polygons
 for route_index, linestring in enumerate(routes):
     candidate_indices = list(index.intersection(linestring.bounds))
     for polygon, metadata in [(polygons[x], polygon_metadata[x]) for x in candidate_indices]:
         try:
             route_in_polygon = linestring.intersection(polygon)
             if not route_in_polygon.is_empty:
-                # add the linestring metadata to polygon metadata
-                metadata.update(route_metadata[route_index])
-                add_to_new_linestrings(route_in_polygon, metadata)
+                # check if we matched preferred polygon
+                for key, preferred_values in preferred_polygon_filter.items():
+                    if metadata[key] in preferred_values:
+                        # add the linestring metadata to polygon metadata
+                        metadata.update(route_metadata[route_index])
+                        add_to_new_linestrings(route_in_polygon, metadata)
+                        break
+                else:
+                    # if there was no preferred match, leave unmatched for now
+                    add_to_remaining_routes(route_index, route_in_polygon)
                 # remove the discovered section from any further matching
                 linestring = linestring.difference(polygon)
                 routes[route_index] = linestring
@@ -159,33 +168,35 @@ routes = list(remaining_routes)
 remaining_routes = [[] for route in routes]
 
 
-# 2) cut according to any ylre boundaries crossed, to get more granular match for further processing
-for route_index, route in enumerate(routes):
-    for line_index, linestring in enumerate(route):
-        candidate_indices = list(complete_index.intersection(linestring.bounds))
-        # first, split those parts of linestring that are within an area
-        for polygon, metadata in [(all_polygons[x], all_polygon_metadata[x]) for x in candidate_indices]:
-            try:
-                route_in_polygon = linestring.intersection(polygon)
-                if not route_in_polygon.is_empty:
-                    add_to_remaining_routes(route_index, route_in_polygon)
-                    # remove the discovered section from any further matching
-                    linestring = linestring.difference(polygon)
-            except TopologicalError:
-                print('Ignoring invalid polygon')
-        # finally, add the parts that didn't belong to a polygon
-        add_to_remaining_routes(route_index, linestring)
+# # 2) cut according to any ylre boundaries crossed, to get more granular match for further processing
+# for route_index, route in enumerate(routes):
+#     for line_index, linestring in enumerate(route):
+#         candidate_indices = list(complete_index.intersection(linestring.bounds))
+#         # first, split those parts of linestring that are within an area
+#         for polygon, metadata in [(all_polygons[x], all_polygon_metadata[x]) for x in candidate_indices]:
+#             try:
+#                 route_in_polygon = linestring.intersection(polygon)
+#                 if not route_in_polygon.is_empty:
+#                     add_to_remaining_routes(route_index, route_in_polygon)
+#                     # remove the discovered section from any further matching
+#                     linestring = linestring.difference(polygon)
+#             except TopologicalError:
+#                 print('Ignoring invalid polygon')
+#         # finally, add the parts that didn't belong to a polygon
+#         add_to_remaining_routes(route_index, linestring)
+#
+# print("Cut remaining parts of routes by underlying geometries")
+# routes = list(remaining_routes)
+# remaining_routes = [[] for route in routes]
 
-print("Cut remaining parts of routes by underlying geometries")
-routes = list(remaining_routes)
-remaining_routes = [[] for route in routes]
 
-
-# 3) match nearby bike lanes
+# 2) match nearby bike lanes
 def add_the_polygon_with_most_overlap(route_index, line_index, polygon_indices, overlaps):
     selected_polygon_index = polygon_indices[max(enumerate(map(lambda polygon: polygon.area, overlaps)),
                                                         key=operator.itemgetter(1))[0]]
+    print(polygon_indices)
     print(selected_polygon_index)
+    print(overlaps)
     metadata = polygon_metadata[selected_polygon_index]
     # add the linestring metadata to polygon metadata
     metadata.update(route_metadata[route_index])
@@ -204,15 +215,26 @@ for route_index, route_buffered in enumerate(line_end_buffers):
         nearby_bike_lanes = []
         nearby_bike_lane_indices = []
         candidate_indices = list(index.intersection(ends_buffered.bounds))
+        # the index contains non-preferred polygons too
         for polygon_index, (polygon, metadata) in enumerate([(polygons[x], polygon_metadata[x]) for x in candidate_indices]):
-            buffer_polygon_intersection = ends_buffered.intersection(polygon)
-            if not buffer_polygon_intersection.is_empty:
-                # do not add the intersection if the buffers are over 8 m apart and the polygon doesn't touch both
-                if isinstance(ends_buffered, MultiPolygon) and not isinstance(buffer_polygon_intersection, MultiPolygon):
-                    continue
-                # print('Multipolygon ' + str(buffer_polygon_intersection) + ' found, matching line ' + str(line_index) + ' to polygon ' + str(polygon_index))
-                nearby_bike_lanes.append(buffer_polygon_intersection)
-                nearby_bike_lane_indices.append(candidate_indices[polygon_index])
+            # check if the polygon is preferred
+            for key, preferred_values in preferred_polygon_filter.items():
+                if metadata[key] in preferred_values:
+                    break
+            else:
+                # nah, we don't like the polygon enough
+                continue
+            try:
+                buffer_polygon_intersection = ends_buffered.intersection(polygon)
+                if not buffer_polygon_intersection.is_empty:
+                    # do not add the intersection if the buffers are over 8 m apart and the polygon doesn't touch both
+                    if isinstance(ends_buffered, MultiPolygon) and not isinstance(buffer_polygon_intersection, MultiPolygon):
+                        continue
+                    # print('Multipolygon ' + str(buffer_polygon_intersection) + ' found, matching line ' + str(line_index) + ' to polygon ' + str(polygon_index))
+                    nearby_bike_lanes.append(buffer_polygon_intersection)
+                    nearby_bike_lane_indices.append(candidate_indices[polygon_index])
+            except TopologicalError:
+                print('Ignoring invalid polygon')
         # pick the lane with the most overlap
         if nearby_bike_lanes:
             print(nearby_bike_lane_indices)
@@ -226,33 +248,40 @@ routes = list(remaining_routes)
 remaining_routes = [[] for route in routes]
 
 
-# 4) map to the (car/path/whatever) lane with the most overlap by buffering the whole line:
+# 3) map to the (car/path/whatever) lane with the most overlap by buffering the whole line:
 buffers = [[line.buffer(4.0) for line in route] for route in routes]
 print("Created buffers for remaining parts of routes")
-#
+
 # save the buffer list flattened
 for item in [item for sublist in buffers for item in sublist]:
     buffer_save.write({'geometry': mapping(item), 'properties': OrderedDict([('id', 'mock')])})
 buffer_save.close()
-#
-# # TODO: do this for lanes including bike lanes, car lanes and park paths but *not* pedestrian lanes!
+
 for route_index, route_buffered in enumerate(buffers):
     for line_index, buffer in enumerate(route_buffered):
         nearby_polygons = []
         nearby_polygon_indices = []
         candidate_indices = list(index.intersection(buffer.bounds))
         for polygon_index, (polygon, metadata) in enumerate([(polygons[x], polygon_metadata[x]) for x in candidate_indices]):
-            buffer_polygon_intersection = buffer.intersection(polygon)
-            if not buffer_polygon_intersection.is_empty:
-                nearby_polygons.append(buffer_polygon_intersection)
-                nearby_polygon_indices.append(candidate_indices[polygon_index])
-                # print('Found intersection between ' + str(buffer) + ' and ' + str(polygon))
+            # check if the polygon is ignored
+            for key, ignored_values in ignored_polygon_filter.items():
+                if metadata[key] in ignored_values:
+                    continue
+            try:
+                buffer_polygon_intersection = buffer.intersection(polygon)
+                if not buffer_polygon_intersection.is_empty:
+                    nearby_polygons.append(buffer_polygon_intersection)
+                    nearby_polygon_indices.append(candidate_indices[polygon_index])
+                    # print('Found intersection between ' + str(buffer) + ' and ' + str(polygon))
+            except TopologicalError:
+                print('Ignoring invalid polygon')
         # quick and dirty approximation:
         # pick the one with the most overlap, do not cut the line further
         if nearby_polygons:
             add_the_polygon_with_most_overlap(route_index, line_index, nearby_polygon_indices, nearby_polygons)
+    # if the linestring squarely falls in the midst of ignored polygons, it might show up as a gap in the results.
 
-print("Matched rest of the routes to nearby bike lanes or, failing that, regular lanes")
+print("Matched the rest of the routes to any lanes with the most overlap")
 
 # print("Stitching together pieces that belong together")
 # # TODO: look through the whole data and combine any linestrings with identical metadata!
